a26610e58576a17f1837a52b1441074fe9f29d26
Luiza Serian <151098795+TheSerian@users.noreply.github.com>
2025-09-24 20:48:17 -0300

    fix: removidas as aspas de var.region e var.zone

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "google" {
   project = var.project
-  region  = "var.region"
-  zone    = "var.zone"
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
