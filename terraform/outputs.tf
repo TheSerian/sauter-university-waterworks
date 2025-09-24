9ff3709b51a5849b37338fcc1f163473ac138db8
Farao <luizaserian@gmail.com>
2025-09-24 04:33:27 -0300

    feat: add ci-cd.yml; refactor: add id wif ao outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "github_actions_sa_email" {
   description = "E-mail da Service Account usada pelo Github Actions"
   value       = google_service_account.sa_github_actions.email
 }
+
+output "wif_id_completo" {
+  description = "Full Resource Name do Workload Identity Provider, usado pelo GitHub Actions."
+  value       = google_iam_workload_identity_pool_provider.github_provider.name
+}
