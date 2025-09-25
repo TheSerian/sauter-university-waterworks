6976f2447d289b548091042da7c9f4f8cc207e64
Luiza Serian <151098795+TheSerian@users.noreply.github.com>
2025-09-25 07:49:42 -0300

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -33,13 +33,6 @@ resource "google_project_iam_member" "sa_artifact_registry_admin" {
   member  = "serviceAccount:${google_service_account.sa_github_actions.email}"
 }
 
-# criar tokens (impersonar) a sa_executor
-resource "google_service_account_iam_member" "github_actions_can_impersonate_executor" {
-  service_account_id = google_service_account.sa_executor.name 
-  role               = "roles/iam.serviceAccountTokenCreator"
-  member             = "serviceAccount:${google_service_account.sa_github_actions.email}" 
-}
-
 resource "google_service_account_iam_member" "wi_sa_binding" {
   service_account_id = google_service_account.sa_github_actions.name
   role               = "roles/iam.workloadIdentityUser"
